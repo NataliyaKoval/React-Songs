77af32981f0468017cb3f72455a036e9b7521ba3
NataliiaKoval <hakunata@gmail.com>
2021-02-26 10:41:42 +0200

    songsReducer, selectedSongReducer

---
+++ src/reducers/index.js
@@ -0,0 +1,28 @@
+const songsReducer = () => {
+    return [
+        {
+            title: 'Indestructible';
+            duration: 4.38
+        },
+        {
+            title: 'Immortalized';
+            duration: 4.18
+        },
+        {
+            title: 'What Are You Waiting For';
+            duration: 4.04
+        },
+        {
+            title: 'Down With The Sickness';
+            duration: 4.38
+        }
+    ]
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+}
